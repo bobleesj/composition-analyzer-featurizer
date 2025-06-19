1b0da2dafc432481be2766c5ec391e3f9d8d4472
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-19 22:56:08 +0900

    refactor: remove content of custom sortgs

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,38 +12,7 @@ def oliynyk_obj():
 def oliynyk_db(oliynyk_obj):
     return oliynyk_obj.db
 
-
-@pytest.fixture
-def cif_ICSD_file():
-    return Cif("tests/data/cifs/ICSD_43054.cif")
-
-
 @pytest.fixture
 def cif_PCD_file():
     return Cif("tests/data/cifs/PCD_250117.cif")
 
-
-@pytest.fixture
-def custom_label_excel_path():
-    return "tests/data/sort/test-custom-labels.xlsx"
-
-
-@pytest.fixture
-def custom_labels_from_excel():
-    return {
-        2: {"A": ["Fe", "Co", "Ni"], "B": ["Si", "Ga", "Ge"]},
-        3: {
-            "R": ["Sc", "Y", "La"],
-            "M": ["Fe", "Co", "Ni"],
-            "X": ["Si", "Ga", "Ge"],
-        },
-        4: {
-            "A": ["Sc", "Y", "La"],
-            "B": ["Fe", "Co", "Ni"],
-            "C": ["Si", "Ga", "Ge"],
-            "D": ["Gd", "Tb", "Dy"],
-        },
-    }
-
-
-#
